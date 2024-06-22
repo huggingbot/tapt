2616812a5996aa068f02d4b81917d9007abc80a5
lwin <lwin@tor.us>
2024-06-22 15:06:30 +0800

    added new endpoint to send notifications

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import notificationRoutes from './notifications';
 import orderRoutes from './orders';
 import transactionRoutes from './transactions';
 
@@ -7,5 +8,6 @@ const router = Router();
 
 router.use('/orders', orderRoutes);
 router.use('/transactions', transactionRoutes);
+router.use('/notifications', notificationRoutes);
 
 export default router;
